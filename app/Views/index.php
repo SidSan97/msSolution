29f070e5a3cd87478e256491840806cf64cf35b5
SidSan97 <sidnei1.8santiago@hotmail.com>
2022-03-23 17:36:57 -0300

    Adição do Ajax e datatable

--- app/Views/index.php
+++ app/Views/index.php
@@ -6,8 +6,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <title>Home</title>
+
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<script type="text/javascript" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+		    $('#jquery-datatable-ajax-php').DataTable({
+		      	'processing': true,
+		      	'serverSide': true,
+		      	'serverMethod': 'post',
+		      	'ajax': {
+		          	'url':'http://localhost/msSolution/public/Controller/ClientesController.php'
+		      	},
+		      	'columns': [
+		         	{ data: 'nome' },
+		         	{ data: 'email' },
+		         	{ data: 'cpf' },
+		         	{ data: 'data_cadastro' }
+		      	]
+		   });
+
+		} );
+	</script>
 </head>
 <body>
-    oii
+	<div class="container mt-5">
+		<h2 style="margin-bottom: 30px;">jQuery Datatable Ajax PHP Example</h2>
+		<table id="jquery-datatable-ajax-php" class="display" style="width:100%">
+	        <thead>
+	            <tr>
+	                <th>Nome</th>
+	                <th>Email</th>
+	                <th>CPF</th>
+	                <th>Data do Cadastro</th>
+	            </tr>
+	        </thead>
+	    </table>
+	</div>
 </body>
 </html>
---
+++ public/Controller/ClientesController.php
@@ -0,0 +1,72 @@
+<?php
+   require('../../app/Models/conexao.php');
+   
+   $draw            = $_POST['draw'];
+   $row             = $_POST['start'];
+   $rowperpage      = $_POST['length']; 
+   $columnIndex     = $_POST['order'][0]['column']; 
+   $columnName      = $_POST['columns'][$columnIndex]['data']; 
+   $columnSortOrder = $_POST['order'][0]['dir']; 
+   $searchValue     = $_POST['search']['value']; 
+
+   $searchArray = array();
+
+   // Search
+   $searchQuery = " ";
+   if($searchValue != '')
+   {
+      $searchQuery = " AND (nome LIKE :nome OR 
+           email LIKE :email OR
+           cpf   LIKE :cpf   OR 
+           data_cadastro LIKE :data_cadastro ) ";
+
+      $searchArray = array( 
+           'nome' =>"%$searchValue%",
+           'email'=>"%$searchValue%",
+           'cpf'  =>"%$searchValue%",
+           'data_cadastro'=>"%$searchValue%"
+      );
+   }
+
+   $sql = $conn->prepare("SELECT COUNT(*) AS allcount FROM clientes ");
+   $sql->execute();
+   $records = $sql->fetch();
+   $totalRecords = $records['allcount'];
+
+   $sql = $conn->prepare("SELECT COUNT(*) AS allcount FROM clientes WHERE 1 ".$searchQuery);
+   $sql->execute($searchArray);
+   $records = $sql->fetch();
+   $totalRecordwithFilter = $records['allcount'];
+
+   $sql = $conn->prepare("SELECT * FROM clientes WHERE 1 ".$searchQuery." ORDER BY ".$columnName." ".$columnSortOrder." LIMIT :limit,:offset");
+
+   foreach ($searchArray as $key=>$search) {
+      $sql->bindValue(':'.$key, $search,PDO::PARAM_STR);
+   }
+
+   $sql->bindValue(':limit', (int)$row, PDO::PARAM_INT);
+   $sql->bindValue(':offset', (int)$rowperpage, PDO::PARAM_INT);
+   $sql->execute();
+   $empRecords = $sql->fetchAll();
+
+   $data = array();
+
+   foreach ($empRecords as $row) 
+   {
+      $data[] = array(
+         "nome"  =>$row['nome'],
+         "email" =>$row['email'],
+         "cpf"   =>$row['cpf'],
+         "data_cadastro"=>$row['data_cadastro']
+      );
+   }
+
+   // Response
+   $response = array(
+      "draw" => intval($draw),
+      "iTotalRecords" => $totalRecords,
+      "iTotalDisplayRecords" => $totalRecordwithFilter,
+      "aaData" => $data
+   );
+
+   echo json_encode($response);
